2f4b4759f37b04e4d2300a78eda64ba6c4000502
Manish Raj <manish_raj@outlook.com>
2016-02-28 19:18:29 +0530

    reddit userscript

---
+++ remove_reddit_comments/userscript.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Delete comments
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Delete comments
+// @author       Olibhel
+// @match        https://www.reddit.com/user/<reddit-username>/
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+var delay_ms = 500
+
+window.onload = function(){    
+    var i = 0; 
+    $('.del-button .yes').each(function(){ 
+        var button = $(this); 
+        setTimeout( function(){ 
+            button.click()
+        }, ++i * delay_ms) 
+    })
+    setTimeout(function(){
+        document.location.reload()
+    }, i * delay_ms)
+}
